678bdf074c107c595df5c3c2026065b55d9d5269
Andrei Cristof <andreimcristof@gmail.com>
2022-02-12 22:19:06 +0100

    DI and such

--- projects/messages/src/messages/messages.controller.ts
+++ projects/messages/src/messages/messages.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  constructor(private messagesService: MessagesService) {}
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) throw new NotFoundException(`message ${id} not found.`);
+
+    return message;
+  }
+}
